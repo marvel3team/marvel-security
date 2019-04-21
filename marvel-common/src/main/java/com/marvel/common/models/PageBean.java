fb35c4780f9a4d44b618c970b5a5fed4bd5b0e7a
钟杰 <zhongjie@zhongjiedeMacBook-Pro-2.local>
2019-04-21 23:20:41 +0800

    pagebean

---
+++ marvel-common/src/main/java/com/marvel/common/models/PageBean.java
@@ -0,0 +1,46 @@
+package com.marvel.common.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @Classname PageBean
+ * @Description TODO
+ * @Date 2019/4/21 下午11:13
+ * @Author zhongjie
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageBean<T> implements Serializable {
+
+    /**
+     * 当前页码
+     */
+    private Integer page;
+    /**
+     * 每页数据量
+     */
+    private Integer count;
+    /**
+     * 总记录数
+     */
+    private Integer total;
+    /**
+     * 当前游标值
+     */
+    private Long cursor;
+    /**
+     * 下一页游标值
+     */
+    private Long nextCursor;
+    /**
+     * 结果集
+     */
+    private List<T> list;
+
+}
