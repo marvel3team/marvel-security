dea91d940bf3acfd62d3661ceab6c60f359faf5e
lizhui <happyxiaolizi@126.com>
2019-04-24 23:48:19 +0800

    新增科局员工接口

--- marvel-web/src/main/java/com/marvel/web/mapper/BureauMapper.java
+++ marvel-web/src/main/java/com/marvel/web/mapper/BureauMapper.java
@@ -4,6 +4,7 @@ import com.marvel.web.po.Bureau;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -28,4 +29,11 @@ public interface BureauMapper {
             "</script>"
     })
     List<Bureau> getBureauByIds(@Param("list") List<Long> bureauIds);
+
+
+    @Select("select id,ares_id,company_id,name,mobile,remark from t_bureau_info where id = #{id}")
+    Bureau getBureauById(@Param("id") Long id);
+
+    @Update("update t_bureau_info set ares_id=#{param.aresId},company_id=#{param.companyId},name=#{param.name},mobile=#{param.mobile},remark=#{param.remark} where id=#{param.id}")
+    int update(@Param("param") Bureau bureau);
 }
