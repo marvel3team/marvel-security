366922979e501e8d3e175dca361a7743358f8441
钟杰 <zj17@meitu.com>
2019-06-23 20:13:21 +0800

    fix

--- marvel-web/src/main/java/com/marvel/web/vo/RemoteInfoReqVo.java
+++ marvel-web/src/main/java/com/marvel/web/vo/RemoteInfoReqVo.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -16,7 +17,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class RemoteInfoReqVo {
+public class RemoteInfoReqVo implements Serializable {
 
     /**
      * 计划id
